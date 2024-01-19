ad4634379b6d980bd7d2d42adc5ebb4c5f62be6f
Matthias Hryniszak <padcom@gmail.com>
2024-01-19 09:56:44 +0100

    Initial import

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <HelloWorld :type="type" />
+  <select v-model="type">
+    <option value="text">Text</option>
+    <option value="date">Date</option>
+    <option value="multiline">Multiline</option>
+  </select>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+const type = ref('text')
+</script>
+
+<style lang="postcss" scoped>
+.note {
+  margin-top: 2.5rem;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <input v-if="type === 'text'" ref="input" type="text" @focus="focus">
+  <input v-else-if="type === 'date'" ref="input" type="date" @focus="focus">
+  <textarea v-else-if="type === 'multiline'" ref="input" @focus="focus" />
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+defineProps({
+  type: { type: String, default: 'text' },
+})
+
+const input = ref<HTMLElement>()
+
+function focus() {
+  // @ts-ignore xxx
+  console.log(input.value?.tagName, input.value?.attributes.type)
+}
+</script>
+
+<style lang="postcss" scoped>
+h1 {
+  font-size: 1.875rem;
+  line-height: 2.25rem;
+}
+</style>
